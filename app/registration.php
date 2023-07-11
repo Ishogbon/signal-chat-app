979011864dc363ac7b838b50e84c86794b5a0c17
Daniel Shogbon <juanmichael179@gmail.com>
2023-07-11 23:20:44 +0100

    Added authentication system and improved RWD

--- app/registration.php
+++ app/registration.php
@@ -18,7 +18,7 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        <form>
+        <form id="registration-form">
             <div id="auth-page-header">
                 <h1> Sign Up for ReText </h1>
                 <p>Sign up now and start texting family and friends</p>
@@ -27,26 +27,30 @@
                 <div id="site-name">
                     <span>ReText</span>
                 </div>
+                <div id="form-error-bar">
+                    
+                </div>
                 <div id="first-name" class="registration-input-bars">
-                    <input placeholder="First Name" type="text" class="registration-form-input" />
+                    <input name="first_name" placeholder="First Name" type="text" class="registration-form-input" maxlength="50"/>
                 </div>
                 <div id="surname" class="registration-input-bars">
-                    <input placeholder="Last Name" type="text" class="registration-form-input" />
+                    <input name="last_name" placeholder="Last Name" type="text" class="registration-form-input"  maxlength="50"/>
                 </div>
                 <div id="handle-tag" class="registration-input-bars">
-                    <input placeholder="Handle Tag" type="text" class="registration-form-input" />
+                    <input name="handle_tag" placeholder="Handle Tag" type="text" class="registration-form-input" maxlength="50" required/>
                 </div>
                 <div id="email" class="registration-input-bars">
-                    <input placeholder="Email" type="text" class="registration-form-input" />
+                    <input name="email" placeholder="Email" type="email" class="registration-form-input" maxlength="255" required/>
                 </div>
                 <div id="password" class="registration-input-bars">
-                    <input placeholder="Password" type="password" class="registration-form-input" />
+                    <input name="password" placeholder="Password" type="password" class="registration-form-input" minlength="8" required/>
                 </div>
+                <p id="other-reg">Already have an account? <a href="./login.php">Sign In</a></p>
                 <div id="submit-button" class="registration-button-bars">
                     <button class="registration-button">Register</button>
                 </div>
             </div>
         </form>
-        <script src="" async defer></script>
+        <script src="./scripts/site/authentications.js" async defer></script>
     </body>
 </html>
